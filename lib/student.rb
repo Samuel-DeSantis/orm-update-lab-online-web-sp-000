2f2c33685111ce5fe2a6ce9cdb36bd189389ca1f
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-07 04:52:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE name = ?
     SQL
-    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
   end
 
   def update
