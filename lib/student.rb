91c30203a26e9dba0b2428c963665b4117ec61f8
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-07 04:07:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,4 +28,30 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def self.create
+
+  end
+
+  def self.new_from_db(row) #=> ['id','name','grade']
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
 end
